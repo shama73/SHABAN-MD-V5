84533b0d518051f2401f49cd3c6f59cbbdbd2089
shama73 <deploybot86@gmail.com>
2025-06-10 08:39:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~mFETCTCJ#MuxHRSxE9NouVwq0EwZ9VYXgy5e_3zEj2TdsTOUUeKo",
 // add your Session Id
 AUTO_BIO: process.env.AUTO_BIO || "false", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
